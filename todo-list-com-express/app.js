c8227a38bbe23ec0ead1516bc1672805a2458901
wesantos <goneisgone@hotmail.com>
2023-01-02 15:00:22 -0300

    Middlewares

--- todo-list-com-express/app.js
+++ todo-list-com-express/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const log = (req,res, next) => {
+    console.log(req.body);
+    console.log(`data: ${Date.now()}`);
+    next();
+}
+
+app.use(log)
+
 app.get('/', (req,res) => {
     res.send('<h1>Minha Lista de tarefas</h1>')
 })
 
 app.get('/json', (req,res) => {
+    console.log(req.body);
     res.json({
         title: 'Estudar MERN',
         done: true
