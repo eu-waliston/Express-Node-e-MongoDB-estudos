d8959552cc924fb091fd0fc6defcf43f5d4f1085
wesantos <goneisgone@hotmail.com>
2023-01-02 10:25:55 -0300

    Entrada e saida de dados utilizando STDIN e STDOUT

--- entrada-e-saida-de-Dados/io.js
+++ entrada-e-saida-de-Dados/io.js
@@ -1,1 +1,9 @@
-const process 
+const process = require('process');
+
+console.log(`Digite seu nome`);
+
+process.stdin.on('data', (keyboard) => {
+    process.stdout.write(`Texto do usuario: ${keyboard}`)
+    process.exit();
+})
+
