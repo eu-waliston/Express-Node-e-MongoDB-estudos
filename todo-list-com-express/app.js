7c54bf2b5ca5e50941d318c28a3a497a94cda14f
wesantos <goneisgone@hotmail.com>
2023-01-02 11:09:08 -0300

    Resposta JSON

--- todo-list-com-express/app.js
+++ todo-list-com-express/app.js
@@ -5,6 +5,13 @@ app.get('/', (req,res) => {
     res.send('<h1>Minha Lista de tarefas</h1>')
 })
 
+app.get('/json', (req,res) => {
+    res.json({
+        title: 'Estudar MERN',
+        done: true
+    })
+})
+
 app.listen(3000, () => {
     console.log(`server running!`);
 })
