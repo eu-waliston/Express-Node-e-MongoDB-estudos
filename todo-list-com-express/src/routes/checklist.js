15bb58fbf4ad908f5ea3bd181cb1272a18dcaf09
wesantos <goneisgone@hotmail.com>
2023-01-02 15:30:34 -0300

    Parametros de rotas

--- todo-list-com-express/src/routes/checklist.js
+++ todo-list-com-express/src/routes/checklist.js
@@ -12,4 +12,10 @@ router.post('/', (req,res) => {
     res.status(200).send(req.body);
 })
 
+router.get('/:id', (req,res) => {
+    console.log(req.params.id);
+    res.send(`ID: ${req.params.id}`)
+})
+
+
 module.exports = router
