6ca5536c699f77843782d66c53bbe338c6d57f85
wesantos <goneisgone@hotmail.com>
2023-01-02 15:35:18 -0300

    PUT e DELETE

--- todo-list-com-express/src/routes/checklist.js
+++ todo-list-com-express/src/routes/checklist.js
@@ -17,5 +17,15 @@ router.get('/:id', (req,res) => {
     res.send(`ID: ${req.params.id}`)
 })
 
+router.put('/:id', (req,res) => {
+    console.log(req.params.id);
+    res.send(` PUT ID: ${req.params.id}`)
+})
+
+router.delete('/:id', (req,res) => {
+    console.log(req.params.id);
+    res.send(` DELETE: ${req.params.id}`)
+})
+
 
 module.exports = router
