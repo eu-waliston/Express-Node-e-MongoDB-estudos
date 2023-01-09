76fdd9280cc064d98157b32ab209379312282b4a
walwizard <goneisgone@hotmail.com>
2023-01-09 19:15:08 -0300

    EJS e configuração com o Express

--- todo-list-com-express/app.js
+++ todo-list-com-express/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const checklistRouter = require('./src/routes/checklist')
-require('./config/database')
+const path = require('path');
 
+const checklistRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+require('./config/database');
 
 const app = express();
 app.use(express.json());
 
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+
+app.use('/', rootRouter);
 app.use('/checklists', checklistRouter);
 
 app.listen(3000, () => {
---
+++ todo-list-com-express/src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router();
+
+router.get('/' , async (req,res) => {
+    res.render('pages/index');
+});
+
+
+module.exports = router;
