d91cca13662632211e8b7dc45524c12211f49618
walwizard <goneisgone@hotmail.com>
2023-01-13 10:08:05 -0300

    Deletando uma task

--- todo-list-com-express/app.js
+++ todo-list-com-express/app.js
@@ -22,6 +22,7 @@ app.set('view engine', 'ejs');
 app.use('/', rootRouter);
 app.use('/checklists', checklistRouter);
 app.use('/checklists', taskRouter.checklistDependent);
+app.use('/tasks', taskRouter.simple);
 
 app.listen(3000, () => {
     console.log(`server running!`);
--- todo-list-com-express/src/routes/task.js
+++ todo-list-com-express/src/routes/task.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const checklistDependentRoute = express.Router();
+const simpleRouter = express.Router();
 
 const Checklist = require("../models/checklist");
 const Task = require("../models/task");
@@ -18,27 +19,40 @@ checklistDependentRoute.get("/:id/tasks/new", async (req, res) => {
   }
 });
 
+simpleRouter.delete("/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    const checklist = await Checklist.findById(task.checklist);
+    const taskToRemove = checklist.tasks.indexOf(task._id);
+    checklist.tasks.splice(taskToRemove, 1);
+    checklist.save();
+    res.redirect(`/checklists/${checklist._id}`);
+  } catch (error) {
+    res.status(500).render('pages/error', { error: 'Error deleting task. ' + error });
+  }
+});
 
-checklistDependentRoute.post('/:id/tasks', async(req,res) => {
-    let { name } = req.body.task;
-    let task = new Task({ name, checklist: req.params.id });
+checklistDependentRoute.post("/:id/tasks", async (req, res) => {
+  let { name } = req.body.task;
+  let task = new Task({ name, checklist: req.params.id });
 
-    try {
-        await task.save();
-        let checklist = await Checklist.findById(req.params.id);
+  try {
+    await task.save();
+    let checklist = await Checklist.findById(req.params.id);
 
-        checklist.tasks.push(task);
-        await checklist.save();
-        res.redirect(`/checklists/${req.params.id}`)
-        
-    } catch (error) {
-        let errors = error.errors;
-        res.status(422).render("tasks/new", {
-          task: { ...task, errors },
-          checklistId: req.params.id,
-        });
-    }
+    checklist.tasks.push(task);
+    await checklist.save();
+    res.redirect(`/checklists/${req.params.id}`);
+  } catch (error) {
+    let errors = error.errors;
+    res.status(422).render("tasks/new", {
+      task: { ...task, errors },
+      checklistId: req.params.id,
+    });
+  }
+});
 
-})
-
-module.exports = { checklistDependent: checklistDependentRoute };
+module.exports = {
+  checklistDependent: checklistDependentRoute,
+  simple: simpleRouter,
+};
