b532640e098f9cc58f756f3fcd5737ea6edc1864
walwizard <goneisgone@hotmail.com>
2023-01-13 09:31:39 -0300

    Cadastro de uma nova tarefa (Parte 2)

--- todo-list-com-express/src/routes/checklist.js
+++ todo-list-com-express/src/routes/checklist.js
@@ -52,7 +52,7 @@ router.post("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    let checklist = await Checklist.findById(req.params.id);
+    let checklist = await Checklist.findById(req.params.id).populate('tasks');
     res.status(200).render("checklists/show", { checklist: checklist });
   } catch (error) {
     res
--- todo-list-com-express/src/routes/task.js
+++ todo-list-com-express/src/routes/task.js
@@ -5,10 +5,12 @@ const checklistDependentRoute = express.Router();
 const Checklist = require("../models/checklist");
 const Task = require("../models/task");
 
-checklistDependentRoute.get("/:id/tasks/new", (req, res) => {
+checklistDependentRoute.get("/:id/tasks/new", async (req, res) => {
   try {
     let task = Task();
-    res.status(200).render('tasks/new', {checklistId: req.params.id, task: task})
+    res
+      .status(200)
+      .render("tasks/new", { checklistId: req.params.id, task: task });
   } catch (error) {
     res
       .status(422)
@@ -17,4 +19,26 @@ checklistDependentRoute.get("/:id/tasks/new", (req, res) => {
 });
 
 
-module.exports = {checklistDependent: checklistDependentRoute}
+checklistDependentRoute.post('/:id/tasks', async(req,res) => {
+    let { name } = req.body.task;
+    let task = new Task({ name, checklist: req.params.id });
+
+    try {
+        await task.save();
+        let checklist = await Checklist.findById(req.params.id);
+
+        checklist.tasks.push(task);
+        await checklist.save();
+        res.redirect(`/checklists/${req.params.id}`)
+        
+    } catch (error) {
+        let errors = error.errors;
+        res.status(422).render("tasks/new", {
+          task: { ...task, errors },
+          checklistId: req.params.id,
+        });
+    }
+
+})
+
+module.exports = { checklistDependent: checklistDependentRoute };
